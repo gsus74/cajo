2db3154b4134f3f07b50ac13b37c7a5cdfa44b4e
njesus <nuno.m.jesus@gmail.com>
2016-06-24 23:16:03 +0100

    Learning project

---
+++ academy/academy/academy.cpp
@@ -0,0 +1,20 @@
+// academy.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int v1, v2;
+	
+	cin >> v1 >> v2;
+
+	cout << v1 + v2 << endl;
+
+	return 0;
+}
+
